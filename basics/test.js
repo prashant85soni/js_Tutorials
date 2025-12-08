cc6d0b53fd33cf94191fedb73f12bd0c507e421f
prashant85soni <88302892+prashant85soni@users.noreply.github.com>
2025-12-08 08:33:46 +0000

    check_var

--- basics/test.js
+++ basics/test.js
@@ -1,2 +1,11 @@
 console.log("Prashant Soni")
 console.log("New Add")
+const a=78;
+var x=56;
+let check=45;
+c1="hello"
+console.table([a,x,check,c1])
+//a=23;
+var x=57
+let check1=67;
+console.table([a,x,check,c1])
